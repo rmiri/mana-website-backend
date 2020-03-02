f2089f68617343e23d61e306222e86b19b482352
sarafotros <sarafotros@gmail.com>
2020-03-02 12:03:39 +0000

    CORS error resolved

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ Bundler.require(*Rails.groups)
 
 module ManaWebsite
   class Application < Rails::Application
+
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
@@ -33,5 +35,14 @@ module ManaWebsite
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+      
+
   end
+
 end
