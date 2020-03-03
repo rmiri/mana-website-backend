d804e512a526365862b95284c552948a48c2f228
sarafotros <sarafotros@gmail.com>
2020-03-03 15:34:22 +0000

    product images seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,9 @@ Service.delete_all
 u1 = User.create(name:"Renata",family_name:"Freitas",email:"email@fakeemail.com",password:"123", phone:"123123123",dob:"1995-04-19",admin?:true)
 u2 = User.create(name:"Toli",family_name:"Benson",email:"email@fakeemail.com",password:"123", phone:"123123123",dob:"1990-01-10",admin?:false)
 
-p1 = Product.create(name:"A.H.A", price:250, image:"", quantity:"120", description:"night cream")
-p2 = Product.create(name:"Anti-againg", price:150, image:"", quantity:"220", description:"day cream")
-p3 = Product.create(name:"FilorgaHydra", price:350, image:"", quantity:"200", description:"filler")
+p1 = Product.create(name:"A.H.A", price:250, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_400/v1583248941/filorganight_duxzkl.jpg", quantity:"120", description:"night cream")
+p2 = Product.create(name:"Sleep And Peele", price:150, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorga-sleepnpeel_i04bxy.jpg", quantity:"220", description:"Night cream")
+p3 = Product.create(name:"Filorga Optim Eye", price:350, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorgaEye_hjnptv.jpg", quantity:"200", description:"filler")
 
 Service.create(name:"Hydra", description:"hydration", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231332/eyeImage_xvoa7r.jpg", price:"100")
 Service.create(name:"rejuvenating", description:"anti-aging", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231333/handServiceImage_xxx4zn.jpg", price:"110")
