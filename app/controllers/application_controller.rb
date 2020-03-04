099c8fd05e7f27b9b5b8b97ba1bd10ae1d0bf1d9
sarafotros <sarafotros@gmail.com>
2020-03-04 11:39:26 +0000

    session thing created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::API
 
-
-    def generate_token(date)
-        JWT.encode(data, )
+    def secret 
+        ENV["MANA"]
     end
+
+    def generate_token(data)
+        JWT.encode(data, secret)
+    end
+
+    def decode_token 
+        token = request.headers["AUTHORIZATION"]
+        begin
+        JWT.decode(token, secret).first 
+        rescue
+            {}
+        end 
+    end
+
+    def get_user 
+        id = decode_token["id"]
+        User.find_by(id: id)
+    end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def login 
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-            render json: {user: user }
+            render json: {user: user, token: generate_token({id: user.id }) }
         else
             render json: { message: "Invalid email/password"}
         end
