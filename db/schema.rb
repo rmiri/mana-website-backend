dbbe59318835fc4610d72104b48bbde73fe326ba
rmiri <re_miriuk@hotmail.com>
2020-03-05 11:11:12 +0000

    category column added to products and services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_141027) do
+ActiveRecord::Schema.define(version: 2020_03_05_105228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_141027) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "promotions", force: :cascade do |t|
@@ -72,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_141027) do
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,20 @@ u1 = User.create(name:"Renata",family_name:"Freitas",email:"email@fakeemail.com"
 u2 = User.create(name:"Toli",family_name:"Benson",email:"email2@fakeemail.com",password:"123", phone:"123123123",dob:"1990-01-10",admin?:false)
 u3 = User.create(name:"Sara",family_name:"Fotros",email:"sara@fakeemail.com",password:"123", phone:"123123123",dob:"1995-04-19",admin?:true)
 
-p1 = Product.create(name:"A.H.A", price:250, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_400/v1583248941/filorganight_duxzkl.jpg", quantity:"120", description:"night cream")
-p2 = Product.create(name:"Sleep And Peele", price:150, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorga-sleepnpeel_i04bxy.jpg", quantity:"220", description:"Night cream")
-p3 = Product.create(name:"Filorga Optim Eye", price:350, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorgaEye_hjnptv.jpg", quantity:"200", description:"filler")
+p1 = Product.create(name:"A.H.A", price:250, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_400/v1583248941/filorganight_duxzkl.jpg", quantity:"120", description:"night cream", category:"Body")
+p2 = Product.create(name:"Sleep And Peele", price:150, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorga-sleepnpeel_i04bxy.jpg", quantity:"220", description:"Night cream",category:"Face")
+p3 = Product.create(name:"Filorga Optim Eye", price:350, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorgaEye_hjnptv.jpg", quantity:"200", description:"filler",category:"Eye")
+p4 = Product.create(name:"A.H.A", price:250, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_400/v1583248941/filorganight_duxzkl.jpg", quantity:"120", description:"night cream", category:"Body")
+p5 = Product.create(name:"Sleep And Peele", price:150, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorga-sleepnpeel_i04bxy.jpg", quantity:"220", description:"Night cream",category:"Hands")
+p6 = Product.create(name:"Filorga Optim Eye", price:350, image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/c_scale,w_500/v1583248784/filorgaEye_hjnptv.jpg", quantity:"200", description:"filler",category:"Eye")
 
-Service.create(name:"Hydra", description:"hydration", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231332/eyeImage_xvoa7r.jpg", price:"100")
-Service.create(name:"rejuvenating", description:"anti-aging", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231333/handServiceImage_xxx4zn.jpg", price:"110")
-Service.create(name:"hand", description:"hand treatment", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231334/massageHand_xbyora.jpg", price:"50")
+
+
+Service.create(name:"Firming", description:"hydration", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231332/eyeImage_xvoa7r.jpg", price:100,category:"Rejuvenate")
+Service.create(name:"Exfoliating", description:"anti-aging", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231333/handServiceImage_xxx4zn.jpg", price:110,category:"Hands")
+Service.create(name:"Detox", description:"hand treatment", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231334/massageHand_xbyora.jpg", price:50,category:"Detox")
+Service.create(name:"Hydration", description:"hydration", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231332/eyeImage_xvoa7r.jpg", price:100,category:"Hydration")
+Service.create(name:"Eye Treatment", description:"anti-aging", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231333/handServiceImage_xxx4zn.jpg", price:110,category:"Eye")
+Service.create(name:"Massage with Qigong", description:"hand treatment", image:"https://res.cloudinary.com/ddxhjhvdj/image/upload/v1583231334/massageHand_xbyora.jpg", price:50,category:"Body")
 
  
